9e51fecd951f088c6c42daf8682ba1fa6d4e899a
Jessi Hernandez <jessih989@outlook.com>
2016-08-31 21:12:36 -0700

    Update Stage1.py

--- Stage1.py
+++ Stage1.py
@@ -4,14 +4,16 @@
 
 import requests
 
-MYGITHUB_url = 'https://github.com/JessiH989/Code2040FellowsAp' 
-REGISTRATION_endpoint = 'http://challenge.code2040.org/api/register'
-API_token = 'c3ef305c42d60f1ef3eda0527c743cb2'
+def STAGE1_registration():
 
-#JSON Dictionary
-JSON_token = {'github': MYGITHUB_url, 'token': API_token}
+  MYGITHUB_url = 'https://github.com/JessiH989/Code2040FellowsAp' 
+  REGISTRATION_endpoint = 'http://challenge.code2040.org/api/register'
+  API_token = 'c3ef305c42d60f1ef3eda0527c743cb2'
 
-#Connecting MYGITHUB_url to the REGISTRATION_endpoint with my API_token
-r = requests.post(REGISTRATION_endpoint, JSON_token)
+  #JSON Dictionary
+  JSON_token = {'github': MYGITHUB_url, 'token': API_token}
 
-print(r.text)
+  #Connecting MYGITHUB_url to the REGISTRATION_endpoint with my API_token
+  r = requests.post(REGISTRATION_endpoint, JSON_token)
+
+STAGE1_registration()
